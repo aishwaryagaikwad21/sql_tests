bbf9f1ae044a790b32256431c31ddfaf1e45e72c
aishwaryagaikwad21 <aishwaryagaikwad21@gmail.com>
2026-01-03 14:50:40 +0530

    delete rows

--- src3/delete/delete0.sql
+++ src3/delete/delete0.sql
@@ -5,7 +5,10 @@
 DELETE FROM "collections" WHERE "title" = 'Spring outing';
 
 -- Deletes item where value is NULL
-DELETE FROM "collections" WHERE "acquired" IS NULL;
+DELETE FROM "collections" WHERE "acquired" IS NULL; --won't delete if there is an empty string - ''
+
+DELETE FROM "collections"
+WHERE "acquired" = '';
 
 -- Deletes items acquired before the museum moved to a new location in 1909
 DELETE FROM "collections" WHERE "acquired" < '1909-01-01';
